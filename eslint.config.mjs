cb998af48bb0eb0fee89f2730430624a3ba00325
Ashfaq Hussain <asfak@vcbay.co>
2025-01-20 20:26:26 +0530

    init express project

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+// @ts-check
+
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+    {
+        ignores: [
+            'dist',
+            'node_modules',
+            'eslint.config.mjs',
+            'jest.config.js',
+            '**/*.spec.ts',
+            'tests',
+        ],
+    },
+    {
+        languageOptions: {
+            parserOptions: {
+                project: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+        rules: {
+            'no-console': 'warn',
+            '@typescript-eslint/no-unused-vars': 'error',
+            '@typescript-eslint/no-misused-promises': 'off',
+            '@typescript-eslint/require-await': 'warn',
+        },
+    },
+)
---
+++ readme.md
@@ -0,0 +1,9 @@
+Steps 
+- Create .nvmrc 
+- yarn init
+- install ts dev dependency and npx tsc --init
+- rootdir src, outdir 
+- install dev dependency ts-node nodemon
+- Install prettier for code formating exact
+- Eslint setup exact
+- git setup
